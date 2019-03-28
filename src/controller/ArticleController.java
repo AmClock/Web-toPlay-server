48a59e97ef0f53aa2e4b3ee046df51720a872aa4
AmClock <46196960+AmClock@users.noreply.github.com>
2019-03-28 18:22:57 +0900

    $190328
    $190328

--- src/controller/ArticleController.java
+++ src/controller/ArticleController.java
@@ -106,10 +106,10 @@ public class ArticleController {
 	 * @name updateArticle \n
 	 * @brief 게시판 업데이트 함수 \n
 	 * @param referer ==> 전 페이지 URL \n
-	 * @param seqTmp[] ==> 컨텐츠 순서 \n
-	 * @param noTmp[] ==> 컨텐츠 번호 \n
-	 * @param imgTmp[] ==> 이미지 이름 \n
-	 * @param contentTmp[] ==> 컨텐츠 \n
+	 * @param         seqTmp[] ==> 컨텐츠 순서 \n
+	 * @param         noTmp[] ==> 컨텐츠 번호 \n
+	 * @param         imgTmp[] ==> 이미지 이름 \n
+	 * @param         contentTmp[] ==> 컨텐츠 \n
 	 * @return String \n
 	 * @author park
 	 * @version 1.0
@@ -140,7 +140,7 @@ public class ArticleController {
 	/**
 	 * @name updateArticle \n
 	 * @brief 업데이트 페이지로 이동 , 번호의 페이지에서 컨텐츠를 불러준다. \n
-	 * @param no ==> 페이지번호 \n
+	 * @param no      ==> 페이지번호 \n
 	 * @param referer ==> 전 페이지 URL \n
 	 * @param model   \n
 	 * @return String \n
@@ -158,8 +158,8 @@ public class ArticleController {
 	/**
 	 * @name uploadImg \n
 	 * @brief 이미지 업로드 및 이미지 리사이즈 \n
-	 * @param request \n
-	 * @param type \n
+	 * @param request   \n
+	 * @param type      \n
 	 * @param uploadImg \n
 	 * @return String \n
 	 * @author park \n
@@ -201,9 +201,9 @@ public class ArticleController {
 
 	/**
 	 * @name ajaxLike \n
-	 * @brief 버튼을 클릭시 like 기능 처리  \n
+	 * @brief 버튼을 클릭시 like 기능 처리 \n
 	 * @param clickFlag ==> 클릭유무 \n
-	 * @param count ==> count 객체 :: insert시키위한 것 \n
+	 * @param count     ==> count 객체 :: insert시키위한 것 \n
 	 * @return Map \n
 	 * @author park \n
 	 * @version 1.0 \n
@@ -215,12 +215,11 @@ public class ArticleController {
 		return articleService.likeFunction(count, clickFlag);
 	}// ajaxLike end
 
-/*여기까지 했음*/
 	/**
 	 * @name writeReply \n
-	 * @brief 버튼을 클릭시 like 기능 처리  \n
-	 * @param reply \n
-	 * @return Map \n
+	 * @brief 리플을 쓰느 함수 \n
+	 * @param Reply reply \n
+	 * @return int \n
 	 * @author park \n
 	 * @version 1.0 \n
 	 * @see None \n
@@ -231,33 +230,83 @@ public class ArticleController {
 		return articleService.wirteReply(reply);
 	}// wrteReply ebd
 
+	/**
+	 * @name listReply \n
+	 * @brief 리플의 리스트를 불러오는 곳 \n
+	 * @param int pageNo \n
+	 * @param int articleNo \n
+	 * @return Map \n
+	 * @author park \n
+	 * @version 1.0 \n
+	 * @see None \n
+	 */
 	@RequestMapping(value = "/ajax/list/reply/{pageNo}", method = RequestMethod.POST)
 	@ResponseBody
 	public Map<String, Object> listReply(@PathVariable int pageNo, int articleNo) {
 		return articleService.listReply(articleNo, pageNo);
 	}// listReply ebd
 
+	/**
+	 * @name deleteReply \n
+	 * @brief 리플을 삭제하는 함수 \n
+	 * @param int no \n
+	 * @return int \n
+	 * @author park \n
+	 * @version 1.0 \n
+	 * @see None \n
+	 */
 	@RequestMapping(value = "/ajax/delete/reply", method = RequestMethod.POST)
 	@ResponseBody
 	public int deleteReply(int no) {
 		return articleService.deletReply(no);
 	}// listReply ebd
 
+	/**
+	 * @name updateReply \n
+	 * @brief 리플을 수정하는 함수 \n
+	 * @param Reply reply \n
+	 * @return int \n
+	 * @author park \n
+	 * @version 1.0 \n
+	 * @see None \n
+	 */
 	@RequestMapping(value = "/ajax/update/reply", method = RequestMethod.POST)
 	@ResponseBody
 	public int updateReply(Reply reply) {
 		return articleService.updateReply(reply);
 	}// listReply ebd
 
+	/**
+	 * @name articleList \n
+	 * @brief 게시판 리스트 불러오는 함수 \n
+	 * @param        int no \n
+	 * @param String order \n
+	 * @param        int searchNo \n
+	 * @param        int userNo \n
+	 * @return Map \n
+	 * @author park \n
+	 * @version 1.0 \n
+	 * @see None \n
+	 */
 	@RequestMapping(value = "/ajax/list/article", method = RequestMethod.POST)
 	@ResponseBody
 	public Map<String, Object> articleList(int no, String order, int searchNo, int userNo) {
 		return articleService.mainList(no, order, searchNo, userNo);
 	}// listReply ebd
 
+	/**
+	 * @name ajaxSearchTitle \n
+	 * @brief 검색 자동완성 기능의 함수\n
+	 * @param String title \n
+	 * @return List\n
+	 * @author park \n
+	 * @version 1.0 \n
+	 * @see None \n
+	 */
 	@RequestMapping(value = "/ajax/search/title", method = RequestMethod.POST)
 	@ResponseBody
 	public List<Article> ajaxSearchTitle(String title) {
 		return articleService.searchTitle(title);
 	}
+
 }// ArticleController end
--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -19,6 +19,11 @@ import util.FileRenameUtil;
 import util.ResizeImageUtil;
 import vo.User;
 
+/**
+ * @file UserController.java \n
+ * @brief user class \n
+ * @author park \n
+ */
 @Controller
 public class UserController {
 
@@ -26,39 +31,78 @@ public class UserController {
 
 	public void setUserService(UserService userService) {
 		this.userService = userService;
-	}
+	}// setUserService end
 
 	private FileRenameUtil fileRenameUtil;
-	
+
 	public void setFileRenameUtil(FileRenameUtil fileRenameUtil) {
 		this.fileRenameUtil = fileRenameUtil;
-	}
-	
+	}// setFileRenameUtil end
+
 	private ResizeImageUtil resizeImageUtil;
-	
+
 	public void setResizeImageUtil(ResizeImageUtil resizeImageUtil) {
 		this.resizeImageUtil = resizeImageUtil;
-	}
-	
+	}// setResizeImageUtil end
+
+	/**
+	 * @name goMain \n
+	 * @brief 메인 페이지 이동 함수 \n
+	 * @return String \n
+	 * @author park \n
+	 * @version 1.0 \n
+	 * @see None \n
+	 */
 	@RequestMapping(value = { "/", "/main" }, method = RequestMethod.GET)
 	public String goMain() {
 		return "main";
 	}// goMain end
 
+	/**
+	 * @name goSignUp \n
+	 * @brief 가입 페이지 이동 함수 \n
+	 * @param String referer \n
+	 * @params Model model \n
+	 * @return String \n
+	 * @author park \n
+	 * @version 1.0 \n
+	 * @see None \n
+	 */
 	@RequestMapping(value = "/signUp", method = RequestMethod.GET)
 	public String goSignUp(@RequestHeader String referer, Model model) {
 		model.addAttribute("referer", referer);
 		return "signUp";
 	}// goSignUp end
-	
+
+	/**
+	 * @name update \n
+	 * @brief 회원정보 수정 함수 \n
+	 * @param String referer \n
+	 * @params HttpSession session \n
+	 * @params User user \n
+	 * @return String \n
+	 * @author park \n
+	 * @version 1.0 \n
+	 * @see None \n
+	 */
 	@RequestMapping(value = "/signUp", method = RequestMethod.PUT)
-	public String update(User user , @RequestHeader String referer , HttpSession session) {
-		if(userService.userUpdate(user)) {
+	public String update(User user, @RequestHeader String referer, HttpSession session) {
+		if (userService.userUpdate(user)) {
 			System.out.println("referer :: " + referer);
 		}
 		return "redirect:" + referer;
 	}// goSignUp end
-	
+
+	/**
+	 * @name signUpUser \n
+	 * @brief 회원정보 가입 기능 함수 (insert) \n
+	 * @param String referer \n
+	 * @params User user \n
+	 * @return String \n
+	 * @author park \n
+	 * @version 1.0 \n
+	 * @see None \n
+	 */
 	@RequestMapping(value = "/signUp", method = RequestMethod.POST)
 	public String signUpUser(User user, String referer) {
 		String url = "signUp";
@@ -68,73 +112,140 @@ public class UserController {
 		return url;
 	}
 
-	@RequestMapping(value="/ajax/check/id/{id}" , method = RequestMethod.GET)
+	/**
+	 * @name ajaxCheckId \n
+	 * @brief 아이디 체크 함수 \n
+	 * @param String id \n
+	 * @return String \n
+	 * @author park \n
+	 * @version 1.0 \n
+	 * @see None \n
+	 */
+	@RequestMapping(value = "/ajax/check/id/{id}", method = RequestMethod.GET)
 	@ResponseBody
 	public String ajaxCheckId(@PathVariable String id) {
-		return "{\"result\":"+ userService.checkId(id)+"}";
+		return "{\"result\":" + userService.checkId(id) + "}";
 	}
-	
-	@RequestMapping(value="/ajax/check/name/{name}",method=RequestMethod.GET)
+
+	/**
+	 * @name ajaxCheckName \n
+	 * @brief 닉네임 체크 함수 \n
+	 * @param String id \n
+	 * @return String \n
+	 * @author park \n
+	 * @version 1.0 \n
+	 * @see None \n
+	 */
+	@RequestMapping(value = "/ajax/check/name/{name}", method = RequestMethod.GET)
 	@ResponseBody
 	public String ajaxCheckName(@PathVariable String name) {
-		return "{\"result\":"+userService.checkName(name)+"}";
+		return "{\"result\":" + userService.checkName(name) + "}";
 	}
-	
+
+	/**
+	 * @name goLogin \n
+	 * @brief 로그인 페이지 이동 함수 \n
+	 * @param String referer \n
+	 * @param Model  model \n
+	 * @return String \n
+	 * @author park \n
+	 * @version 1.0 \n
+	 * @see None \n
+	 */
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String goLogin(@RequestHeader String referer, Model model) {
 		model.addAttribute("referer", referer);
 		return "login";
 	}// goSignUp end
 
+	/**
+	 * @name login \n
+	 * @brief 로그인 페이지 이동 함수 \n
+	 * @param String      referer \n
+	 * @param User        user \n
+	 * @param HttpSession session \n
+	 * @return String \n
+	 * @author park \n
+	 * @version 1.0 \n
+	 * @see None \n
+	 */
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String login(String referer, User user, HttpSession session) {
 		session.setAttribute(User.LOGIN, userService.login(user));
 		return "redirect:" + referer;
 	}// login end
-	
-	
-	@RequestMapping(value="/login",method=RequestMethod.DELETE)
+
+	/**
+	 * @name login \n
+	 * @brief 로그아웃 함수\n
+	 * @param HttpSession session \n
+	 * @return String \n
+	 * @author park \n
+	 * @version 1.0 \n
+	 * @see None \n
+	 */
+	@RequestMapping(value = "/login", method = RequestMethod.DELETE)
 	public String login(HttpSession session) {
 		session.invalidate();
 		return "redirect:/main";
 	}
 
-	@RequestMapping(value="/ajax/upload",method=RequestMethod.POST , produces="application/json;charset=UTF-8")
+	/**
+	 * @name uploadProfile \n
+	 * @brief 프로필 이미지 저장 및 리사이즈 함수\n
+	 * @param HttpServletRequest request \n
+	 * @param String             type \n
+	 * @param MultipartFile      uploadImg \n
+	 * @return String \n
+	 * @author park \n
+	 * @version 1.0 \n
+	 * @see None \n
+	 */
+	@RequestMapping(value = "/ajax/upload", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
 	@ResponseBody
 	public String uploadProfile(HttpServletRequest request, String type, MultipartFile uploadImg) {
 		ServletContext sc = request.getServletContext();
-		
+
 		String uploadPath = sc.getRealPath("img/upload");
 		String profilePath = sc.getRealPath("img/profile");
-		
-		File file = new File(uploadPath+File.separator+uploadImg.getOriginalFilename()); 
-		
-		file = fileRenameUtil.rename(file);		
-		
+
+		File file = new File(uploadPath + File.separator + uploadImg.getOriginalFilename());
+
+		file = fileRenameUtil.rename(file);
+
 		String src = "";
-	
+
 		try {
 			uploadImg.transferTo(file);
-			switch(type) {
-			case "P" : 
-				resizeImageUtil.resize(file.getAbsolutePath(),profilePath+File.separator+file.getName(), 200);
+			switch (type) {
+			case "P":
+				resizeImageUtil.resize(file.getAbsolutePath(), profilePath + File.separator + file.getName(), 200);
 				break;
-			case "B" : 
-				src+="upload/";
+			case "B":
+				src += "upload/";
 				break;
-			}//switch end
-			src+= file.getName();
-			return "{\"src\":\""+src+"\"}";
+			}// switch end
+			src += file.getName();
+			return "{\"src\":\"" + src + "\"}";
 		} catch (Exception e) {
 			e.printStackTrace();
-			return "에러";
+			return "�뿉�윭";
 		}
-	}//uploadProfile end
-	
-	@RequestMapping(value="/update" , method = RequestMethod.GET)
+	}// uploadProfile end
+
+	/**
+	 * @name update \n
+	 * @brief 업데이트 페이지로 이동하는 함수\n
+	 * @param Model m \n
+	 * @return String \n
+	 * @author park \n
+	 * @version 1.0 \n
+	 * @see None \n
+	 */
+	@RequestMapping(value = "/update", method = RequestMethod.GET)
 	public String update(Model m) {
-		m.addAttribute("update","test");
+		m.addAttribute("update", "test");
 		return "signUp";
 	}
-	
+
 }// UserController end
